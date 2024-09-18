3e4749a52a6515933e8998bd8b7531087cf846a2
GeauxWeisbeck4 <andrew.weisbeck@gmail.com>
2024-09-18 00:45:45 -0400

    Saving changes
    U: Updated footer links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress(),]
+  site: "https://tarheeldevstudio.netlify.app",
+  integrations: [tailwind(), compress(), sitemap(), robotsTxt()]
 });
